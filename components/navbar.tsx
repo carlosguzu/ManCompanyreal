0261916a6f323079219b54de5e9686e46523a3d8
Carlos Guzmán <guzmanc@utb.edu.co>
2023-10-21 19:35:55 -0500

    logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -52,7 +52,8 @@ export const Navbar = () => {
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
-						<Logo />
+						
+						
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
