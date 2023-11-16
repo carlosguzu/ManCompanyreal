1e462d3a9fe2b8f2502bc1d877a10851df84b63e
Carlos Guzmán <guzmanc@utb.edu.co>
2023-11-16 18:16:42 -0500

    company orange

--- components/primitives.ts
+++ components/primitives.ts
@@ -14,7 +14,7 @@ export const title = tv({
 			green: "from-[#6FEE8D] to-[#17c964]",
 			pink: "from-[#FF72E1] to-[#F54C7A]",
 			foreground: "dark:from-[#FFFFFF] dark:to-[#4B4B4B]",
-			orange: "from-[#FFA500] to-[#FFD700]",
+			orange: "from-[#FB8914] to-[#FFD700]",
 		},
 		size: {
 			sm: "text-3xl lg:text-4xl",
