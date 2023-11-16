894613b6d221bcfb813a71452a16ebb69a7ef8a5
Carlos Guzmán <guzmanc@utb.edu.co>
2023-11-16 18:20:24 -0500

    search gone

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -30,21 +30,8 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
+
+
 
 	return (
 		
@@ -78,11 +65,9 @@ export const Navbar = () => {
 				className="hidden sm:flex basis-1/5 sm:basis-full"
 				justify="end"
 			>
-				<NavbarItem className="hidden sm:flex gap-2">
+
 				
 					<ThemeSwitch />
-				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
 				<NavbarItem className="hidden md:flex">
 
 				</NavbarItem>
