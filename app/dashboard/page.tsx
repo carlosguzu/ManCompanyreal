0ed4a923eb110f9a7b110e9e3d8f842c9be01e10
Carlos Guzmán <guzmanc@utb.edu.co>
2023-11-21 08:47:18 -0500

    eyeicon.jsx to tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,101 +1,39 @@
 "use client";
-import { useUser  } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 import { title } from "@/components/primitives";
 import React from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue} from "@nextui-org/react";
-import {EditIcon} from "../EditIcon";
-import {DeleteIcon} from "../DeleteIcon";
-import EyeIcon from "../EyeIcon";
-function DashboardPage() {
-	const { isLoaded, isSignedIn, user } = useUser();
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+} from "@nextui-org/react";
+import { EditIcon } from "../EditIcon";
+import { DeleteIcon } from "../DeleteIcon";
+import OrderTable from "@/components/ordertable";
+/*import EyeIcon from "../EyeIcon";*/
 
+export default function DashboardPage() {
+	const { isLoaded, isSignedIn, user } = useUser();
 	
-	  
-	const rows = [
-		{
-		  key: "1",
-		  name: "Tony Reichert",
-		  role: "CEO",
-		  status: "Active",
-		},
-		{
-		  key: "2",
-		  name: "Zoey Lang",
-		  role: "Technical Lead",
-		  status: "Paused",
-		},
-		{
-		  key: "3",
-		  name: "Jane Fisher",
-		  role: "Senior Developer",
-		  status: "Active",
-		},
-		{
-		  key: "4",
-		  name: "William Howard",
-		  role: "Community Manager",
-		  status: "Vacation",
-		},
-	  ];
-	  
-	  const columns = [
-		{
-		  key: "name",
-		  label: "NAME",
-		},
-		{
-		  key: "role",
-		  label: "ROLE",
-		},
-		{
-		  key: "status",
-		  label: "STATUS",
-		},
-	  ];
-  
-	
-	  if (!isLoaded || !isSignedIn) {
+	if (!isLoaded || !isSignedIn) {
 		return null; /*no entra pq esta página está escondida si no tienes acceso y te redirige a /.*/
 	  }
 	  
-	return (
-
-		<section>
+  return (
+	
+    <section className="py-24">
 		<div>
 			<h1 className={title()}>Dashboard</h1>
 			<p>Welcome, {user.firstName}</p>
     </div>
-	<div>
-    <Table aria-label="Example table with dynamic content ">
-      <TableHeader columns={columns}>
-        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-      </TableHeader>
-      <TableBody items={rows}>
-        {(item) => (
-          <TableRow key={item.key}>
-            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-	</div>
-	</section>
+      <div className="container">
+		<OrderTable/>
+      </div>
+    </section>
+	
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-export default DashboardPage;
-
-
-
-
-
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,8 +49,7 @@ export const Navbar = () => {
   
 	const handleMenuToggle = () => {
 	  setIsMenuOpen(!isMenuOpen);
-	  
-	};
+	  	};
 
 	const { theme } = useTheme();
 
