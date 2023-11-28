903bce571280122222b00df33f58a034a3c0ef68
Carlos Guzmán <guzmanc@utb.edu.co>
2023-11-28 13:50:48 -0500

    pie workin

--- components/pie.tsx
+++ components/pie.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import {useOrdersData} from "./data";
+import { useOrdersData } from "./data";
 import * as echarts from 'echarts';
 
 const Pie: React.FC = () => {
-  const {rowlen} = useOrdersData();  
+  const { rowlen } = useOrdersData();
+
   useEffect(() => {
     const chartDom = document.getElementById('pie-chart')!;
     const myChart = echarts.init(chartDom);
@@ -12,63 +13,54 @@ const Pie: React.FC = () => {
       value: row.key,
       name: row.label,
     }));
-  //key y label es igual a value and name, son intercambiables y depende de la biblioteca con la que se esté trabajando
-   
-  const color = {}//???
-
- 
 
     const option = {
-      
-    tooltip: {
-      trigger: 'item'
-    },
-    legend: {
-      top: '5%',
-      left: 'center'
-    },
-    series: [
-      {
-        name: 'Access From',
-        type: 'pie',
-        radius: ['40%', '70%'],
-        avoidLabelOverlap: false,
-        itemStyle: {
-          borderRadius: 10,
-          borderColor: '#fff',
-          borderWidth: 2
-        },
-        label: {
-          show: false,
-          position: 'center'
-        },
-        emphasis: {
+      tooltip: {
+        trigger: 'item',
+      },
+      legend: {
+        top: '5%',
+        left: 'center',
+      },
+      series: [
+        {
+          name: 'Order Status',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2,
+          },
           label: {
-            show: true,
-            fontSize: 40,
-            fontWeight: 'bold'
-          }
+            show: false,
+            position: 'center',
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 14,
+              fontWeight: 'bold',
+            },
+          },
+          labelLine: {
+            show: false,
+          },
+          data,
         },
-        labelLine: {
-          show: false
-        },
-        data,
-      }
-    ]
+      ],
     };
 
-    
     option && myChart.setOption(option);
 
-      // Cleanup when the component unmounts
-      return () => {
-        myChart.dispose();
-      };
-    }, []);
-  
-    return <div id="pie-chart" style={{ height: '400px', width: '290px' }} />;
-  };
-  
-  export default Pie;
+    // Cleanup when the component unmounts
+    return () => {
+      myChart.dispose();
+    };
+  }, [rowlen]);
 
-  
+  return <div id="pie-chart" style={{ height: '400px', width: '300px' }} />;
+};
+
+export default Pie;
